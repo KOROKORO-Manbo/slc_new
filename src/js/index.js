6ff6e311acdf054e81d1185c48526d3c7d2ac3fe
KOROKORO <pingping_hiromi@yahoo.co.jp>
2018-01-25 21:18:25 +0900

    trivial change

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ require('imports-loader?this=>window!../calendar/jquery.sliderPro.min.js')
 
 import '../css/index.css'
 import './jquery.js'
-// import './jquery.inview.min.js'    バンドルで動作しなくなるのでbundle.jsの最後尾のWebPackの外に置いた
+// import './jquery.inview.min.js'    //バンドルで動作しなくなるのでbundle.jsの最後尾のWebPackの外に置いた
 import './main.js'
 import '../calendar/jquery.sliderPro.min.js'
 import './jquery.prettyPhoto.js'
