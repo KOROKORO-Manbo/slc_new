f2a2df5648dbd5320b73e865d18681b2277eb9f5
KOROKORO <pingping_hiromi@yahoo.co.jp>
2018-02-03 11:39:16 +0900

    change inView in index.js to execute import func style

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,20 @@
 require('imports-loader?this=>window!../calendar/jquery.sliderPro.min.js')
-// require('imports-loader?this=>window!./jquery.inview.min.js')
+require('imports-loader?this=>window!./effect/modernizr.custom.56320.js')
+require('imports-loader?this=>window!./effect/sncjm-min.js')
 // require('imports-loader?this=>window!./jquery.pretty_main.js')
 
 import '../css/index.css'
 import './jquery.js'
-// import './jquery.inview.min.js'    バンドルで動作しなくなるのでbundle.jsの最後尾のWebPackの外に置いた
+import myInview from './jquery.inview.min.js'
 import './main.js'
 import '../calendar/jquery.sliderPro.min.js'
 import './jquery.prettyPhoto.js'
 import './jquery.pretty_main.js'
+
+
+myInview();
+//effect
+// import './effect/modernizr.custom.56320.js'
+// import './effect/sncjm-min.js'
+// import './effect/jquery.easing.1.3.js'
+// import './effect/main.js'
